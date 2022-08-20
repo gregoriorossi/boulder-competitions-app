022ef1fc1cb5a937d1ad7d413d5ac851c28b4433
Rossi Gregorio <gr.rossi@reply.it>
2022-08-20 09:54:20 +0200

    tdp-boulder-competitions-app - implemtnato il routing e struttura WIP

--- frontend/tdp-boulder-competitions-app/src/app/app.module.ts
+++ frontend/tdp-boulder-competitions-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { CompetitionsComponent } from './components/competitions/competitions.component';
+import { AthletesComponent } from './components/athletes/athletes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompetitionsComponent,
+    HeaderComponent,
+    CompetitionsComponent,
+    AthletesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: CompetitionsComponent },
+      { path: 'atleti', component: AthletesComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
